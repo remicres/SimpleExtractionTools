98d81129f6d7c1c436a3e0a179efa4ccdf10340e
Rémi <remi.cresson@teledetection.fr>
2016-02-12 16:57:43 +0100

    Update simpleextractiontools.remote.cmake
    Add tag

--- simpleextractiontools.remote.cmake
+++ simpleextractiontools.remote.cmake
@@ -2,5 +2,5 @@
 otb_fetch_module(SimpleExtractionTools
   "A set of simple extraction tools"
   GIT_REPOSITORY https://github.com/remicres/otb-simpleextractiontools.git
-  GIT_TAG xxxx
+  GIT_TAG 79a50074b5f897828ec8994fdf3cd1f360c39fcf
 )
