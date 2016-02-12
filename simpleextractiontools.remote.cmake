79a50074b5f897828ec8994fdf3cd1f360c39fcf
Rémi <remi.cresson@teledetection.fr>
2016-02-12 16:56:02 +0100

    Create simpleextractiontools.remote.cmake

---
+++ simpleextractiontools.remote.cmake
@@ -0,0 +1,6 @@
+#Contact: Rémi Cresson <remi.cresson@teledetection.fr>
+otb_fetch_module(SimpleExtractionTools
+  "A set of simple extraction tools"
+  GIT_REPOSITORY https://github.com/remicres/otb-simpleextractiontools.git
+  GIT_TAG xxxx
+)
